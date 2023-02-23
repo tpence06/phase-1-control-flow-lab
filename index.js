fa711e9776ed7dc136adbe1458cb3d0202f13475
tpence06 <tpence06@hotmail.com>
2023-02-22 23:42:07 -0700

    pass tests

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  if (someNumber <= 400) {
+    return 'This one is on me!'
+  }
+  if (someNumber > 400, someNumber < 2000) {
+    return 'That will be twenty bucks.'
+  }
+  if (someNumber > 2000, someNumber < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  if (someNumber > 2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  if (city === 'NYC') {
+    return ('Ok, sounds good.')
+} 
+else {
+  return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip === "generous") {
+    return "Thank you so much.";
+} else if (tip === "not as generous") {
+    return "Thank you.";
+} else {
+    return "Bye.";
+}
 }
